079f542c8ea88aa9a9fbd1f1d273cd55b1b2e087
scharkhzarrin <62974754+scharkhzarrin@users.noreply.github.com>
2020-04-07 21:14:00 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+document.getElementById("button1").addEventListener("click", function(){
+        document.getElementById("box").style.height = "250px";
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+    document.getElementById("box"). style.backgroundColor = "Blue";
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+    document.getElementById("box"). style.height = "0px";
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+    document.getElementById("box"). style.backgroundColor = "Orange";
+    document.getElementById("box").style.height= "150px";
+});
